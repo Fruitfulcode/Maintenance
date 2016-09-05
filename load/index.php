9c3ea6962a384c62a6723484c5f4c8bd4d0e0141
Vitaliy <ckonurin@gmail.com>
2016-09-05 11:40:15 +0300

    Fixed metadescription

--- load/index.php
+++ load/index.php
@@ -5,9 +5,8 @@
 	if (!empty($mess_arr[0])) $ebody_class = 'error';
 	$mt_options = mt_get_plugin_options(true);
 	$page_title = wp_kses_post($mt_options['page_title']);
-	$description = wp_kses_post($mt_options['description']);
 	if (empty($page_title)) $page_title = get_bloginfo('title');
-	if (empty($description)) $description = get_bloginfo('description');
+	$description = get_bloginfo('description');
 ?>
 <!DOCTYPE html>
 <!--[if IE 7]>
