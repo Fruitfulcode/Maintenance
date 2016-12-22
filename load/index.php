fe88f497b40c2a167fab74773596b7e55a13e602
Artyom Yakovlev <ManiakMill@gmail.com>
2016-12-22 12:58:50 +0200

    Suggestion. Add og meta content.

--- load/index.php
+++ load/index.php
@@ -2,10 +2,30 @@
 	$mess_arr 	 = array();
 	$ebody_class = null;
 	$mess_arr = get_custom_login_code(); 
-	if (!empty($mess_arr[0])) $ebody_class = 'error';
-	$mt_options = mt_get_plugin_options(true);
-	$page_title = get_bloginfo('title');
-	$description = get_bloginfo('description');
+	if (!empty($mess_arr[0])) {
+		$ebody_class = 'error';
+	}
+	$mt_options  = mt_get_plugin_options(true);
+	$site_title  = get_bloginfo('title');
+	$site_description = get_bloginfo('description');
+	
+	
+	$page_title = (isset($mt_options['page_title']) && !empty($mt_options['page_title'])) ? esc_attr($mt_options['page_title']) : null;
+	$logo 		= (isset($mt_options['logo']) && !empty($mt_options['logo'])) ? esc_attr($mt_options['logo']) : null;
+	$logo_ext   = null;
+	
+	if (!empty($logo)) {
+		$logo = wp_get_attachment_image_src($logo, 'full');
+		$logo = esc_url($logo[0]);
+		$logo_info 	= getimagesize($logo);
+		$logo_ext 	= image_type_to_extension($logo_info[2]);
+		$logo_ext = str_replace('.', '', $logo_ext);
+	}
+	
+	$page_description = (isset($mt_options['description']) && !empty($mt_options['description'])) ? esc_attr($mt_options['description']) : $site_description;
+	if (!empty($page_description)) {
+		$page_description = urlencode_deep(apply_filters( 'wpautop', wp_kses_post(stripslashes($page_description))));
+	}
 ?>
 <!DOCTYPE html>
 <!--[if IE 7]>
@@ -21,12 +41,19 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<?php get_page_title(esc_attr($mess_arr[0])); ?>
 	<meta name="viewport" content="width=device-width, user-scalable=no, maximum-scale=1, initial-scale=1, minimum-scale=1">
-	<meta name="description" content="<?php echo $description; ?>"/>
-	<meta property="og:title" content="<?php echo $page_title; ?>"/>
+	<meta name="description" content="<?php echo $site_description; ?>"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta property="og:site_name" content="<?php echo $site_title . ' - ' . $site_description; ?>"/>
+	<meta property="og:title" content="<?php echo $page_title; ?>"/>
 	<meta property="og:type" content="Maintenance"/>
 	<meta property="og:url" content="<?php echo site_url(); ?>"/>
-	<meta property="og:description" content="<?php echo $description; ?>"/>
+	<meta property="og:description" content="<?php echo $page_description; ?>"/>
+	<?php if (!empty($logo)) { ?>
+	<meta property="og:image" content="<?php echo $logo; ?>" />
+	<meta property="og:image:url" content="<?php echo $logo; ?>"/>
+	<meta property="og:image:secure_url" content="<?php echo $logo; ?>"/>
+	<meta property="og:image:type" content="<?php echo $logo_ext; ?>"/>
+	<?php } ?>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php do_action('load_custom_scripts'); ?>
