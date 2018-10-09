db29ce2b0777d522b05f7cefc8fa6576cca11cb4
Yurii Pavlov <yurii.pavlov@fruitfulcode.com>
2018-10-09 15:33:16 +0300

    version update

--- maintenance.php
+++ maintenance.php
@@ -3,9 +3,9 @@
 	Plugin Name: Maintenance
 	Plugin URI: http://wordpress.org/plugins/maintenance/
 	Description: Take your website for maintenance away from public view. Use maintenance plugin if your website is in development or you need to change a few things, run an upgrade. Make it only accessible by login and password. Plugin has a options to add a logo, background, headline, message, colors, login, etc. Extended PRO with more features version is available for purchase.
-	Version: 3.6.1
+	Version: 3.6.2
 	Author: fruitfulcode
-	Author URI: http://fruitfulcode.com
+	Author URI: https://fruitfulcode.com
 	License: GPL2
 */
 /*  Copyright 2013  Fruitful Code  (email : mail@fruitfulcode.com)
