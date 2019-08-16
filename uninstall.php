4aefa4737eab33a33ebb60bac674540e9edb8dc3
Iesyk <esyk@matesmarketing.com>
2019-08-16 14:38:26 +0300

    Optimized code

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,7 @@
 <?php
 
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
-
-delete_option('maintenance_options');
-delete_option('maintenance_db_version');
-
-?>
+}
+delete_option( 'maintenance_options' );
+delete_option( 'maintenance_db_version' );
